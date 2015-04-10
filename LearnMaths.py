1295e2e11995bc967fc117adec875b8786c78e6d
Jason Odoom <jason@ohdoom.net>
2015-04-09 21:58:04 -0400

    Initial upload

---
+++ LearnMaths.py
@@ -0,0 +1,89 @@
+def LearnToAdd ():
+    '''Gives user random addition problems.'''
+    import random
+    while True:
+        n1 = random.randint(0,9)
+        n2 = random.randint (9,20)
+        print('What is the sum of \n:')
+        problems = input(str(n1) + "+" + str(n2) + "= ")
+        if int(problems) == n1 + n2:
+            print('Correct!')
+        else:
+            print('That is incorrect, try again.')
+        learn = input('Would you like to learn more?')
+        if learn != 'yes':
+             break
+
+#print(LearnToAdd())
+
+def LearnToMultiply ():
+    '''Gives user random multiplication problems.'''
+    import random
+    while True:
+        n1 = random.randint(0,9)
+        n2 = random.randint (9,20)
+        print('What is the sum of \n:')
+        problems = input(str(n1) + "*" + str(n2) + "= ")
+        if int(problems) == n1 * n2:
+            print('Correct!')
+        else:
+            print('That is incorrect, try again.')
+        learn = input('Would you like to learn more?')
+        if learn != 'yes':
+             break
+
+#print(LearnToMultiply())
+
+
+def LearnToDivide ():
+    '''Gives user random division problems.'''
+    import random
+    while True:
+        n1 = random.randint(0,9)
+        n2 = random.randint (9,20)
+        print('What is the sum of \n:')
+        problems = input(str(n1) + "/" + str(n2) + "= ")
+        if int(problems) == n1 / n2:
+            print('Correct!')
+        else:
+            print('That is incorrect, try again.')
+        learn = input('Would you like to learn more?')
+        if learn != 'yes':
+             break
+
+#print(LearnToMultiply())
+
+
+def LearnToSubtract ():
+    '''Gives user random subtraction problems.'''
+    import random
+    while True:
+        n1 = random.randint(0,9)
+        n2 = random.randint (9,20)
+        print('What is the sum of \n:')
+        problems = input(str(n2) + "-" + str(n1) + "= ") #Subtraction so bigger # first as we do not want a neg
+        if int(problems) == n2 - n1:
+            print('Correct!')
+        else:
+            print('That is incorrect, try again.')
+        learn = input('Would you like to learn more?')
+        if learn != 'yes':
+             break
+
+#print(LearnToSubtract())
+
+
+def LearnMaths():
+    '''Takes input from user of what math operation they want to learn.
+Choices are: +, -, * and /'''
+    Learn = input('What would you like to learn? ')
+    if Learn in ['Addition', 'addition', 'add', 'Add', 'adding', 'Adding']:
+        print(LearnToAdd())
+    elif Learn in ['subtraction', 'Subtraction', 'Subtract', 'subtract']:
+            print(LearnToSubtract())
+    elif Learn in ['Multiply', 'multiply', 'multiplication', 'Multiplication']:
+                print(LearnToMultiply())
+    elif Learn in ['divide', 'Divide']:
+                    print (LearnToDivide())
+print (LearnMaths())
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Gives user math problems based on input. Choice of operations are multiplication (*), division (/), subtraction (-) and addition (+). 
